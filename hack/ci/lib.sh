8e74c1efd88e39d462e1f4411dc741e5cd9739bb
Paul Holzinger <pholzing@redhat.com>
2026-07-07 14:33:20 +0200

    ci: make the automation release renovate managed
    Put the variable in the workflow file like the renovate config expects,
    see https://github.com/podman-container-tools/automation/pull/29
    
    To ensure the local runs keep working we need a fall back to parse the
    file manually.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- hack/ci/lib.sh
+++ hack/ci/lib.sh
@@ -111,3 +111,9 @@ function remove_packaged_podman_files() {
         sudo rm -f "$fullpath"
     done
 }
+
+# Reads and prints the AUTOMATION_RELEASE value from the workflow file.
+# Used for local runs where ci.sh is not triggered by gh actions.
+function get_automation_release() {
+    sed -En 's/.*AUTOMATION_RELEASE:\s(.*)/\1/p' .github/workflows/lima.yml
+}
